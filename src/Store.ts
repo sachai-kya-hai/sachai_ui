7d42748169b44fa3b7180251917bf33694f1b95b
hassanzareef <103948091+hassanzareef@users.noreply.github.com>
2023-07-29 21:57:36 -0400

    2 add redux (#14)
    * resolve merge conflicts
    
    * Store updated
    
    * resolve merge conflicts
    
    * resolve merge conflicts
    
    * change eslint error to warn
    
    * workflows
    
    * workflows
    
    * workflows
    
    ---------
    
    Co-authored-by: fahad <fahadu8@gmail.com>

--- src/Store.ts
+++ src/Store.ts
@@ -1,6 +1,8 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore} from '@reduxjs/toolkit' ;
+import profileReducer from './slices/ProfileSlice'
 
 const reducer = {
+  profile: profileReducer
 }
 
 export const store = configureStore({reducer})
--- src/slices/ProfileSlice.ts
+++ src/slices/ProfileSlice.ts
@@ -1,5 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { IProfile } from '../Interfaces/IProfile';
+import axios from 'axios';
 
 interface ProfileState {
   loading: boolean;
@@ -12,6 +13,48 @@ const initialProfileState: ProfileState = {
   error: false
 };
 
+//All endpoints tbd, all mock for now
+export const getById = createAsyncThunk(
+  'profile/userid',
+  async (id: number) => {
+    try {
+      const res = await axios.get(
+        `http://localhost:8080/profile/user?userId=${id}`
+      );
+      console.log(res.data);
+      return res.data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
+
+export const getAllProfiles = createAsyncThunk(
+  'profile/getall',
+  async thunkAPI => {
+    try {
+      const res = await axios.get('http://localhost:8080/profile/all');
+      console.log(res.data);
+      return res.data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
+
+export const createProfile = createAsyncThunk(
+  'profile/create',
+  async (prof: IProfile, thunkAPI) => {
+    try {
+      const res = await axios.post('http://localhost:8080/profile/', prof);
+      console.log(res.data);
+      return res.data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+);
+
 export const profileSlice = createSlice({
   name: 'profile',
   initialState: initialProfileState,
@@ -23,8 +66,9 @@ export const profileSlice = createSlice({
       state.profile = undefined;
     }
   },
-  //eslint-disable-next-line
   extraReducers: builder => {
     //axios calls once set up, will be set here through builders and cases.
   }
 });
+
+export default profileSlice.reducer;
